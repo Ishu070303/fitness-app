b53053e9516a8f16057a0a1a56db369497ba039c
Eshika rawat <eshikarawat02@gmail.com>
2023-05-16 20:36:28 +0530

    adde neccessary dependecncies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // it comes from @types/nodes
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }], //it gonna replace ../../ to @ 
+  }
 })
